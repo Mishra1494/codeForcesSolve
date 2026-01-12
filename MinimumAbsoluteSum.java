4783b241f419c9569300a6abdd681736991716cd
Mishra1494 <mishraraunak117@gmail.com>
2026-01-12 22:37:43 +0530

    done  minimumm absolute sum

---
+++ MinimumAbsoluteSum.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class MinimumAbsoluteSum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int length = sc.nextInt();
+            int[] arr = new int[length];
+            for(int i = 0; i < length; i++){
+                arr[i] = sc.nextInt();
+            }
+            if(arr[0] != -1 && arr[length-1] != -1){
+                System.out.println(Math.abs(arr[0] - arr[length-1]));
+            }else if(arr[0] ==  -1 && arr[length-1] == -1){
+                arr[0] = 0;
+                arr[length-1] = 0;
+                System.out.println(0);
+            }else if(arr[0] != -1 && arr[length-1] == -1){
+                arr[length-1] = arr[0];
+                System.out.println(0);
+            }else if(arr[0] == -1 && arr[length-1] != -1){
+                arr[length-1] = arr[length-1];
+                System.out.println(0);
+            }
+            for(int i = 0; i<length; i++){
+                if(arr[i] == -1){
+                    arr[i] = 0;
+                }
+            }
+            for(int i = 0; i < length; i++){
+                System.out.println(arr[i]);
+            }
+
+        }
+    }
+}
